ed0ff3b65fa4029ae737cf6d1347d07b875690e7
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-13 19:16:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    require_once __DIR__ . '/src/Kito/Loader/AbstractLoader.php';
+    require_once __DIR__ . '/lib/Kito/Loader/AbstractLoader.php';
 
-    require_once __DIR__ . '/src/Kito/Loader/PSR0Loader.php';
-    $cacheLoader = new \Kito\Loader\PSR0Loader(__DIR__ . DIRECTORY_SEPARATOR . 'src');
+    require_once __DIR__ . '/lib/Kito/Loader/PSR0Loader.php';
+    $cacheLoader = new \Kito\Loader\PSR0Loader(__DIR__ . DIRECTORY_SEPARATOR . 'lib');
     $cacheLoader->register();
 
-    require_once __DIR__ . '/src/Kito/Loader/BLKLoader.php';
+    require_once __DIR__ . '/lib/Kito/Loader/BLKLoader.php';
     $blkLoader = new \Kito\Loader\BLKLoader($cacheLoader);
     $blkLoader->register();
 
