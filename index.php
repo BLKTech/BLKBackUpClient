755de9e87e1bc69a4ece4cfbf117ff7b3179e3e2
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-13 17:20:03 -0300

    src bug

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once __DIR__ . '/src/Kito/Loader/AbstractLoader.php';
 
     require_once __DIR__ . '/src//Kito/Loader/PSR0Loader.php';
-    $cacheLoader = new \Kito\Loader\PSR0Loader(__DIR__);
+    $cacheLoader = new \Kito\Loader\PSR0Loader(__DIR__ . '/src');
     $cacheLoader->register();
 
     require_once __DIR__ . '/src/Kito/Loader/BLKLoader.php';
