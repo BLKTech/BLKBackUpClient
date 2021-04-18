9a7f63a80c02d806721ad779e23d387ed0b87fdb
TheKito <11139894+TheKito@users.noreply.github.com>
2021-04-18 03:26:51 -0300

    Update common.php

--- common.php
+++ common.php
@@ -8,32 +8,3 @@
     require_once __DIR__ . '/lib/Kito/Loader/BLKLoader.php';
     $blkLoader = new \Kito\Loader\BLKLoader($cacheLoader);
     $blkLoader->register();
-
-    //** FIX ME, FIND BEST WAY TO GET UID FROM HARDWARE **//
-    function getIdMachine(): string
-    {        
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            return sha1(trim(shell_exec("wmic path win32_computersystemproduct get uuid /value")));
-        } else {
-            return sha1(trim(shell_exec("cat /etc/machine-id")));
-        }        
-    }
-
-    //** FIX ME, FIND BEST WAY TO SCAN DRIVES ON WINDOWS **//
-    function getRootDirs(): array
-    {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $drives = array();
-            foreach (range('A', 'Z') as $drive)
-            {
-                if(is_dir($drive . ":" . DIRECTORY_SEPARATOR))
-                {
-                    $drives[] = $drive . ":" . DIRECTORY_SEPARATOR;
-                }
-            }
-            return $drives;
-        } else {
-            return array(DIRECTORY_SEPARATOR);
-        }
-    }
-    
